f8c2abd85e84053b75ffa4b1ed64d3f6f44a8b6f
Junaed <junaed1995@gmail.com>
2023-03-13 20:40:42 +0600

    Initial Commit

---
+++ Food-Mela iOS/Food_Mela_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Mela_iOSApp.swift
+//  Food-Mela iOS
+//
+//  Created by Junaed Muhammad Chowdhury on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_Mela_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
